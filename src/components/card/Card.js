e4b63c81140eb468d48c965f1eb87484875016f0
georgevictor90 <georgevictorl@yahoo.com>
2022-11-24 19:35:41 +0200

    wrap Card image and text in separate containers

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,23 +4,27 @@ import { MdOutlineFavoriteBorder, MdOutlineFavorite } from "react-icons/md";
 function Card(props) {
   return (
     <div className="card" id={props.id}>
-      {/* get favoriteStatus from props and conditionally render heart icon */}
-      <MdOutlineFavoriteBorder className="card-favorite-icon" />
+      <figure>
+        {/* get favoriteStatus from props and conditionally render heart icon */}
+        <MdOutlineFavoriteBorder className="card-favorite-icon" />
 
-      {/* get img src from props  */}
-      <img src={props.image} alt={props.title} />
+        {/* get img src from props  */}
+        <img src={props.image} alt={props.title} />
+      </figure>
 
-      <span className="card-title">{props.title}</span>
+      <div className="card-text">
+        <span className="card-title">{props.title}</span>
 
-      {/* get stockStatus from props */}
-      <span className="card-stock">in stock</span>
+        {/* get stockStatus from props */}
+        <span className="card-stock">in stock</span>
 
-      {/* get price from props */}
-      <span className="card-price">{props.price} EUR</span>
+        {/* get price from props */}
+        <span className="card-price">{props.price} EUR</span>
 
-      <button className="card-button" onClick={props.addToCart}>
-        Add to cart
-      </button>
+        <button className="card-button" onClick={props.addToCart}>
+          Add to cart
+        </button>
+      </div>
     </div>
   );
 }
