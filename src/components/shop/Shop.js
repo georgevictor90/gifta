abca9b6df6dc6214e4fd4b9293959d14b7fa7b06
Victor Lacatus <georgevictorl@yahoo.com>
2023-03-16 01:22:34 +0200

    implement search bar

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, createContext } from "react";
 import { getAllProductsFromApi } from "../../api";
 import Card from "../card/Card";
+import SearchBar from "../searchbar/SearchBar";
 import Sidebar from "../sidebar/Sidebar";
 export const ProductsContext = createContext(null);
 
@@ -11,6 +12,9 @@ function Shop() {
   const [allProducts, setAllProducts] = useState([]);
   const [displayedProducts, setDisplayedProducts] = useState([]);
   const [favorites, setFavorites] = useState(favoritesFromLocalStorage);
+  const [selectedCategory, setSelectedCategory] = useState("all products");
+  const [error, setError] = useState("");
+  const [loadingSpinner, setLoadingSpinner] = useState(false);
 
   useEffect(() => {
     localStorage.setItem("favorites", JSON.stringify(favorites));
@@ -30,6 +34,29 @@ function Shop() {
     setDisplayedProducts(allProducts);
   }, [allProducts]);
 
+  useEffect(() => {
+    if (selectedCategory === "favorites") return;
+    if (selectedCategory === "all products") {
+      setDisplayedProducts(allProducts);
+    } else {
+      setDisplayedProducts(
+        allProducts.filter((prod) => prod.category === selectedCategory)
+      );
+    }
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    setLoadingSpinner(false);
+    if (!allProducts.length) {
+      setError("Loading products");
+      setLoadingSpinner(true);
+    } else if (!favorites.length && selectedCategory === "favorites") {
+      setError("You have no items added to favorites");
+    } else if (!displayedProducts.length) {
+      setError("No products match your search criteria");
+    }
+  }, [allProducts, displayedProducts, favorites]);
+
   const cardElements = displayedProducts.map((product) => {
     return <Card key={product.id} product={product} />;
   });
@@ -44,27 +71,41 @@ function Shop() {
           setAllProducts,
           displayedProducts,
           setDisplayedProducts,
+          selectedCategory,
+          setSelectedCategory,
         }}
       >
         <Sidebar />
-        {cardElements.length ? (
-          <div data-testid="cards-container" className="cards-container">
-            {cardElements}
-          </div>
-        ) : (
-          <div className="no-products">
-            {favorites.length ? (
-              <>
-                <p>Getting products</p>
+
+        <div className="shop-right">
+          <SearchBar />
+          {cardElements.length ? (
+            <div data-testid="cards-container" className="cards-container">
+              {cardElements}
+            </div>
+          ) : (
+            // <div className="no-products">
+            //   {selectedCategory === "favorites" ? (
+            //     <p>You have no favorite items</p>
+            //   ) : (
+            //     <>
+            //       <div className="lds-circle">
+            //         <div></div>
+            //       </div>
+            //       <p>Getting products</p>
+            //     </>
+            //   )}
+            // </div>
+            <div className="no-products">
+              {loadingSpinner && (
                 <div className="lds-circle">
                   <div></div>
                 </div>
-              </>
-            ) : (
-              <p>You have no favorite items</p>
-            )}
-          </div>
-        )}
+              )}
+              <p>{error}</p>
+            </div>
+          )}
+        </div>
       </ProductsContext.Provider>
     </section>
   );
