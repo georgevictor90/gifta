3baaa7f3e45544aa0e1a70516a11da57148b2dc4
Victor Lacatus <georgevictorl@yahoo.com>
2023-03-16 16:06:21 +0200

    refactor cart functions

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,69 +2,50 @@ import React, { useContext } from "react";
 import { CartContext } from "../RouteSwitch";
 import OrderSummary from "../orderSummary/orderSummary";
 import { MdRemoveCircle } from "react-icons/md";
+import QuantityCounter from "../quantityCounter/QuantityCounter";
 
 function Cart() {
   const { cart, setCart } = useContext(CartContext);
 
-  function incrementQuantity(e) {
-    const productId = e.target.parentNode.parentNode.parentNode.id;
+  function updateQuantity(id, count) {
     const newCartItems = cart.map((prod) => {
-      return prod.id.toString() === productId
-        ? { ...prod, quantity: prod.quantity + 1 }
-        : prod;
+      if (prod.id === id) {
+        return { ...prod, quantity: count };
+      }
+      return prod;
     });
     setCart(newCartItems);
   }
 
-  function decrementQuantity(e) {
-    const productId = e.target.parentNode.parentNode.parentNode.id;
-    if (cart.find((prod) => prod.id.toString() === productId).quantity === 1)
-      return;
-    const newCartItems = cart.map((prod) => {
-      return prod.id.toString() === productId
-        ? { ...prod, quantity: prod.quantity - 1 }
-        : prod;
-    });
-    setCart(newCartItems);
-  }
-
-  function removeFromCart(e) {
-    let newCartItems = [...cart];
-    const productId = e.currentTarget.dataset.id;
-    const itemToRemove = cart.find((prod) => prod.id.toString() === productId);
-    const index = cart.indexOf(itemToRemove);
-    newCartItems.splice(index, 1);
-    setCart(newCartItems);
+  function removeFromCart(id) {
+    const newCart = cart.filter((item) => item.id !== id);
+    setCart(newCart);
   }
 
   const cartProducts = cart.map((prod) => {
     return (
       <div key={prod.id} id={prod.id} className="cart-product">
         <img className="cart-product-image" src={prod.image} alt={prod.title} />
-        <div tabIndex="0" className="cart-product-title">
+        <h3 tabIndex="0" className="cart-product-title">
           {prod.title}
-        </div>
+        </h3>
         <div className="cart-product-price">
           <span>Each</span>
           <span>{prod.price} EUR</span>
         </div>
-        <div className="quantity-container">
-          <div className="quantity-counter">
-            <button onClick={decrementQuantity}>-</button>
-            <span>{prod.quantity}</span>
-            <button onClick={incrementQuantity}>+</button>
-          </div>
-        </div>
+
+        <QuantityCounter
+          id={prod.id}
+          updateQuantity={updateQuantity}
+          quantity={prod.quantity}
+        />
+
         <div className="cart-product-total">
           <span>Total</span>
           <span>{(prod.price * prod.quantity).toFixed(2)} EUR</span>
         </div>
 
-        <button
-          data-id={prod.id}
-          onClick={removeFromCart}
-          className="remove-icon"
-        >
+        <button onClick={() => removeFromCart(prod.id)} className="remove-icon">
           <MdRemoveCircle />
         </button>
       </div>
@@ -73,6 +54,7 @@ function Cart() {
 
   return (
     <section className="cart-wrapper">
+      <h1 className="visually-hidden">Your Cart</h1>
       <div className="cart">
         {cart.length ? (
           <div className="items-container">{cartProducts}</div>
